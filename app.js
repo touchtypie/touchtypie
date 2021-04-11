2cd3cb514ddd3c78ae6e4d8d3183540569476a9e
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-04-11 04:00:20 +0000

    Better

--- app.js
+++ app.js
@@ -1,68 +1,144 @@
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(' ', '&nbsp;');
-}
-
-// Model
-var Bubble = function(element, content) {
+var Helpers = function () {
     return {
-        element: element,
-        props: {
-            value: content,
-            characters: content.split('')
+        htmlEntities: function htmlEntities(str) {
+            return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/ /, '&nbsp;');
         }
     };
+}()
+
+// Models
+var Bubble = function (element, value) {
+    var o = {
+        element: element,
+        props: {
+            value: '',
+            characters: []
+        },
+        setValue: function (value) {
+            this.props.value = value;
+            this.props.characters = value.split('')
+        }
+    };
+    o.setValue(value);
+
+    return o;
 };
+var Counter = function (element, value) {
+    var o = {
+        element: element,
+        props: {
+            value: 0,
+        },
+        setValue: function (value) {
+            this.props.value = value;
+        }
+    };
+    o.setValue(value);
+
+    return o;
+}
 
 // Controller
-var BubbleController = function(speechElement, speechHighlightElement, responseElement) {
-    var speech = Bubble(speechElement, speechElement.innerHTML);
-    // var speechHighlight = Bubble(speechHighlightElement, speechHighlightElement.innerHTML);
-    var response = Bubble(responseElement, responseElement.value);  // Textarea value
+var BubbleController = function (o) {
+    var speech = Bubble(o.bubbles[0], o.bubbles[0].innerHTML);
+    var response = Bubble(o.bubbles[1], o.bubbles[1].value); // Textarea value
+    var characterCounter = Counter(o.counters[0], response.props.characters.length);
+    var incorrectCharacterCounter = Counter(o.counters[1], 0);
 
     // Private
-    var characterizeBubble = function(bubble) {
+    var characterizeBubble = function (bubble) {
         // var domElements = [];
         bubble.element.innerHTML = '';
         for (var i = 0; i < bubble.props.characters.length; i++) {
             characterElement = document.createElement('span');
-            characterElement.innerHTML = bubble.props.characters[i];
+            characterElement.innerHTML = Helpers.htmlEntities(bubble.props.characters[i]);
             // domElements.push(characterElement);
             bubble.element.appendChild(characterElement);
         }
     };
-    var markBubbleValid = function(ele, characterIndex) {
+    var markNormalBubble = function (ele, characterIndex) {
+        ele.getElementsByTagName('span')[characterIndex].className = '';
+    }
+    var markInvalidBubble = function (ele, characterIndex) {
+        ele.getElementsByTagName('span')[characterIndex].className = 'invalid';
+    }
+    var markValidBubble = function (ele, characterIndex) {
         ele.getElementsByTagName('span')[characterIndex].className = 'valid';
     }
-    var markBubbleInvalid = function(ele, characterIndex) {
-        spee;ele.getElementsByTagName('span')[characterIndex].className = 'invalid';
-    }
+    var onKeyDown = function (e) {
+        console.log('[onKeyDown]');
 
-    var initBubbles = function() {
-        characterizeBubble(speech);
-    };
-    var initBinds = function() {
-        response.element.addEventListener('onkeyup', function(ele) {
-            // Get
-            response.characters = ele.value; // Textarea
-            // Check
-            for (i = 0; i < response.characters.length; i++) {
-                if (response.characters[i] === speech.characters[i]) {
-                    markBubbleValid(speech, i);
-                }else {
-                    markBubbleInvalid(speech, i);
+        // Data binding
+        var ele = e.target;
+        response.setValue(ele.value); // Textarea
+        characterCounter.setValue(ele.value); // Textarea
+        console.log('[keyup] ele.value: ' + ele.value);
+        console.log('[keyup] ele.value.length: ' + ele.value.length);
+        console.log('[keyup] response.props.characters: ' + response.props.characters);
+        console.log('[keyup] response.props.characters.length: ' + response.props.characters.length);
+
+        // Mark
+        if (response.props.characters.length == 0) {
+            for (var i = 0; i < speech.props.characters.length; i++) {
+                markNormalBubble(speech.element, i);
+            }
+        }
+        if (response.props.characters.length > 0) {
+            var errNum = 0;
+            for (var i = 0; i < response.props.characters.length; i++) {
+                if (response.props.characters[i] !== speech.props.characters[i]) {
+                    markInvalidBubble(speech.element, i);
+                    errNum++;
+                } else {
+                    markValidBubble(speech.element, i);
                 }
             }
-        });
+            for (var i = response.props.characters.length; i < speech.props.characters.length; i++) {
+                markNormalBubble(speech.element, i);
+            }
+
+            characterCounter.setValue(response.props.characters.length);
+            characterCounter.element.getElementsByTagName('span')[0].innerHTML = Helpers.htmlEntities(response.props.characters.length + '/' + speech.props.characters.length);
+
+            incorrectCharacterCounter.setValue(errNum);
+            incorrectCharacterCounter.element.getElementsByTagName('span')[0].innerHTML  = Helpers.htmlEntities(errNum);
+        }
+    }
+
+    var initBinds = function () {};
+    var initBubbles = function () {
+        characterizeBubble(speech);
     };
 
     // Public
-    var init = function() {
+    var init = function () {
         // Event listeners
         initBinds();
 
         // Characterization
         initBubbles()
+    };
 
+    return {
+        init: init,
+        onKeyDown: onKeyDown,
+        response: response
+    };
+}
+
+var HomeworkCounterController = function (homeworkCounterElement) {
+    var homeworkCounter = Counter(homeworkCounterElement, 0);
+
+    var initBinds = function () {};
+
+    var onKeyDown = function() {
+        homeworkCounter.element.innerHTML += 1;
+    }
+
+    // Public
+    var init = function () {
+        // Event listeners
+        initBinds();
     };
 
     return {
@@ -70,14 +146,24 @@ var BubbleController = function(speechElement, speechHighlightElement, responseE
     };
 }
 
-var myApp = function() {
-    var touchtypie = BubbleController(
-        document.getElementsByTagName('speech')[0].getElementsByTagName('content')[0],
-        document.getElementsByTagName('speech')[0].getElementsByTagName('highlight')[0],
-        document.getElementsByTagName('response')[0].getElementsByTagName('textarea')[0]
-    );
+var myApp = function () {
+    var bc = BubbleController({
+        bubbles: [
+            document.getElementsByTagName('speech')[0].getElementsByTagName('content')[0],
+            document.getElementsByTagName('response')[0].getElementsByTagName('textarea')[0],
+        ],
+        counters: [
+            document.getElementsByTagName('counters')[0].getElementsByTagName('charactercounter')[0],
+            document.getElementsByTagName('counters')[0].getElementsByTagName('incorrectcharactercounter')[0],
+        ]
+    });
+    var cc = HomeworkCounterController(
+        document.getElementsByTagName('counters')[0].getElementsByTagName('homeworkcounter')[0],
+    )
+    bc.response.element.addEventListener('keyup', function (e) {
+        bc.onKeyDown(e);
+    });
 
-    console.log(touchtypie)
-    touchtypie.init();
+    console.log(bc)
+    bc.init();
 }();
-
