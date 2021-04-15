77fa4808c84bfae89a3ba32641dfd0a893f195a8
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-04-15 04:36:25 +0000

    Add unitoverall hitpercentagecounter component

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ var BubbleVirtue = function() {
         // Unit
         shot_num_total: 0,
         hit_num_total: 0,
+        hit_num_total_percentage: 0.00,
         miss_num_total: 0,
         amend_num_total: 0,
     };
@@ -114,6 +115,7 @@ var BubbleVirtue = function() {
     var newlife = function() {
         newleaf();
         this.result.hit_num_total = 0;
+        this.result.hit_num_total_percentage = 0.00;
         this.result.miss_num_total = 0;
         this.result.shots_num_total = 0;
         this.result.amend_num_total = 0;
@@ -184,6 +186,7 @@ var Bubble = function(default_value) {
 
             virtue.result.shot_num_total += bubble.value.length == 0 ? 0 : 1;
             virtue.result.hit_num_total += virtue.result.hit_num_new;
+            virtue.result.hit_num_total_percentage = virtue.result.hit_num_total == 0.00 ? 0.00 : (virtue.result.hit_num_total / virtue.result.shot_num_total * 100).toFixed(2);
             virtue.result.miss_num_total += virtue.result.miss_num_new;
             virtue.result.amend_num_total += bubble.value.length < value_length_prev && amend === true ? 1 : 0;
         }
@@ -367,6 +370,13 @@ var BubbleController = function () {
         property: "hit_num_total"
     }).addBinding(
         document.getElementsByTagName('unitoverall')[0].getElementsByTagName('hitcounter')[0].getElementsByTagName('value')[0],
+        'innerHTML'
+    );
+    new Binding({
+        object: _response.virtue.result,
+        property: "hit_num_total_percentage"
+    }).addBinding(
+        document.getElementsByTagName('unitoverall')[0].getElementsByTagName('hitpercentagecounter')[0].getElementsByTagName('value')[0],
         'innerHTML'
     );
     new Binding({
