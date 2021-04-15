4236069b93ee1772b208d3d83918ef45a5615443
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-04-15 12:39:39 +0000

    Add globaloverall amendpercentagecounter component

--- app.js
+++ app.js
@@ -303,6 +303,7 @@ var BubbleController = function () {
             _student.miss_num_total += virtue.result.miss_num_new;
             _student.miss_num_total_percentage = _student.miss_num_total == 0 ? 0.00 : (_student.miss_num_total / _student.shot_num_total * 100).toFixed(2)
             _student.amend_num_total += virtue.result.amend_num_new;
+            _student.amend_num_total_percentage = _student.amend_num_total == 0 ? 0.00 : (_student.amend_num_total / _student.shot_num_total * 100).toFixed(2)
             if (virtue.completed) {
                 // Set hits value
 
@@ -464,6 +465,13 @@ var BubbleController = function () {
         'innerHTML'
     );
     new Binding({
+        object: _student,
+        property: "amend_num_total_percentage"
+    }).addBinding(
+        document.getElementsByTagName('globaloverall')[0].getElementsByTagName('amendpercentagecounter')[0].getElementsByTagName('value')[0],
+        'innerHTML'
+    );
+    new Binding({
         object: _student.virtues,
         property: "count"
     }).addBinding(
@@ -497,6 +505,7 @@ var State = function() {
             miss_num_total: 0,
             miss_num_total_percentage: 0.00,
             amend_num_total: 0,
+            amend_num_total_percentage: 0.00,
             virtues: {
                 count: 0,
                 values: []
